fae4641de92adf201c3957a0a492fcaef177e258
liuqitao <liuqitao@haizhi.com>
2020-07-06 19:49:23 +0800

    fix webui executor tasks count; fix dag

--- core/src/main/resources/org/apache/spark/ui/static/executorspage.js
+++ core/src/main/resources/org/apache/spark/ui/static/executorspage.js
@@ -225,6 +225,7 @@ $(document).ready(function () {
             var allMaxTasks = 0;
             var allActiveTasks = 0;
             var allFailedTasks = 0;
+            var allKilledTasks = 0;
             var allCompletedTasks = 0;
             var allTotalTasks = 0;
             var allTotalDuration = 0;
@@ -247,6 +248,7 @@ $(document).ready(function () {
             var activeMaxTasks = 0;
             var activeActiveTasks = 0;
             var activeFailedTasks = 0;
+            var activeKilledTasks = 0;
             var activeCompletedTasks = 0;
             var activeTotalTasks = 0;
             var activeTotalDuration = 0;
@@ -269,6 +271,7 @@ $(document).ready(function () {
             var deadMaxTasks = 0;
             var deadActiveTasks = 0;
             var deadFailedTasks = 0;
+            var deadKilledTasks = 0;
             var deadCompletedTasks = 0;
             var deadTotalTasks = 0;
             var deadTotalDuration = 0;
@@ -303,6 +306,7 @@ $(document).ready(function () {
                 allMaxTasks += exec.maxTasks;
                 allActiveTasks += exec.activeTasks;
                 allFailedTasks += exec.failedTasks;
+                allKilledTasks += exec.killedTasks;
                 allCompletedTasks += exec.completedTasks;
                 allTotalTasks += exec.totalTasks;
                 allTotalDuration += exec.totalDuration;
@@ -325,6 +329,7 @@ $(document).ready(function () {
                     activeMaxTasks += exec.maxTasks;
                     activeActiveTasks += exec.activeTasks;
                     activeFailedTasks += exec.failedTasks;
+                    activeKilledTasks += exec.killedTasks;
                     activeCompletedTasks += exec.completedTasks;
                     activeTotalTasks += exec.totalTasks;
                     activeTotalDuration += exec.totalDuration;
@@ -347,6 +352,7 @@ $(document).ready(function () {
                     deadMaxTasks += exec.maxTasks;
                     deadActiveTasks += exec.activeTasks;
                     deadFailedTasks += exec.failedTasks;
+                    deadKilledTasks += exec.killedTasks;
                     deadCompletedTasks += exec.completedTasks;
                     deadTotalTasks += exec.totalTasks;
                     deadTotalDuration += exec.totalDuration;
@@ -372,6 +378,7 @@ $(document).ready(function () {
                 "allMaxTasks": allMaxTasks,
                 "allActiveTasks": allActiveTasks,
                 "allFailedTasks": allFailedTasks,
+                "allKilledTasks": allKilledTasks,
                 "allCompletedTasks": allCompletedTasks,
                 "allTotalTasks": allTotalTasks,
                 "allTotalDuration": allTotalDuration,
@@ -395,6 +402,7 @@ $(document).ready(function () {
                 "allMaxTasks": activeMaxTasks,
                 "allActiveTasks": activeActiveTasks,
                 "allFailedTasks": activeFailedTasks,
+                "allKilledTasks": activeKilledTasks,
                 "allCompletedTasks": activeCompletedTasks,
                 "allTotalTasks": activeTotalTasks,
                 "allTotalDuration": activeTotalDuration,
@@ -418,6 +426,7 @@ $(document).ready(function () {
                 "allMaxTasks": deadMaxTasks,
                 "allActiveTasks": deadActiveTasks,
                 "allFailedTasks": deadFailedTasks,
+                "allKilledTasks": deadKilledTasks,
                 "allCompletedTasks": deadCompletedTasks,
                 "allTotalTasks": deadTotalTasks,
                 "allTotalDuration": deadTotalDuration,
@@ -496,6 +505,7 @@ $(document).ready(function () {
                                 }
                             }
                         },
+                        {data: 'killedTasks'},
                         {data: 'completedTasks'},
                         {data: 'totalTasks'},
                         {
@@ -590,6 +600,7 @@ $(document).ready(function () {
                                 }
                             }
                         },
+                        {data: 'allKilledTasks'},
                         {data: 'allCompletedTasks'},
                         {data: 'allTotalTasks'},
                         {
